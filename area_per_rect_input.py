5b1e8b98cb3eaceeaecee66b96894d7ff15e5542
Jeremiah Omoike <jeremiah.omoike@stu.ocsb.ca>
2022-09-30 20:15:03 +0000

    added area_per_rect_input.py

---
+++ area_per_rect_input.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Created by: Jeremiah omoike
+# Created on: September 21 2022
+# This program asks the user for the length and
+# width of the rectangle in mm. It then
+# calculates and displays the area and
+# perimeter.
+import math
+
+
+def main():
+    # input
+    print("Today we will calculate the area and")
+    print("perimeter of a rectangle")
+    length = int(input("Enter the length (cm): "))
+    width = int(input("Enter the width (cm): "))
+
+    # process
+    area = length * width
+    perimeter = 2 * (length + width)
+
+    # output
+    print("")
+    print("Area = {} cm^2".format(area))
+    print("Perimeter = {} cm".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
